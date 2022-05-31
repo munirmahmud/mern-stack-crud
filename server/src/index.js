0cd3b763f987273c28c592dc54c66ad9e15f0def
Munir Mahmud <munirmahmud6@gmail.com>
2022-05-31 19:20:05 +0600

    Deleted file added

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const userRouter = require("./routers/userRouter");
+
 const app = express();
 const PORT = 4000;
 
@@ -10,9 +12,7 @@ require("./db");
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello from Home");
-});
+app.use("/api/user", userRouter);
 
 app.listen(PORT, () => {
   console.log(`Server starts on http://localhost:${PORT}`);
