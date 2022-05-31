688e948f85c134af2ec28b64f79fc8579d7da7c0
Munir Mahmud <munirmahmud6@gmail.com>
2022-05-31 18:42:08 +0600

    Database connection implemented

---
+++ server/src/db/index.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.log(`DB connection failed: ${err}`));
